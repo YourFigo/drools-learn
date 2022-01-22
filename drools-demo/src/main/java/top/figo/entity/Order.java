5380390b055249650c9b7877c1761ed8c9b6d086
Figo <37700763+YourFigo@users.noreply.github.com>
2022-01-22 12:54:55 +0800

    20220122: Add drools-demo

---
+++ drools-demo/src/main/java/top/figo/entity/Order.java
@@ -0,0 +1,36 @@
+package top.figo.entity;
+
+/**
+ * @Author Figo
+ * @Date 2022/1/19 23:28
+ */
+public class Order {
+    //订单原始价格，即优惠前价格
+    private Double originalPrice;
+    //订单真实价格，即优惠后价格
+    private Double realPrice;
+
+    public Double getOriginalPrice() {
+        return originalPrice;
+    }
+
+    public void setOriginalPrice(Double originalPrice) {
+        this.originalPrice = originalPrice;
+    }
+
+    public Double getRealPrice() {
+        return realPrice;
+    }
+
+    public void setRealPrice(Double realPrice) {
+        this.realPrice = realPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "originalPrice=" + originalPrice +
+                ", realPrice=" + realPrice +
+                '}';
+    }
+}
